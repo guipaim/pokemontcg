df1ed535ae1c42e2ca14a5fe6b8a310d258c968e
ln514476 <ln514476@gsk.com>
2024-04-03 19:09:48 -0400

    modified class Useraccount and create users functions. Also, added search users, send friend request and accept friend request .

--- createUser.js
+++ createUser.js
@@ -14,6 +14,8 @@ class UserAccount {
         this.url = mongoConfig.serverUrl;
         this.dbName = mongoConfig.database;
         this.createUser = this.createUser.bind(this);
+        this.sendFriendRequest = this.sendFriendRequest.bind(this);
+        this.acceptFriendRequest = this.acceptFriendRequest.bind(this);
     }
   
    /**
@@ -24,28 +26,92 @@ class UserAccount {
     * @param {*} username needs to be unique string
     * @param {*} password must be string 
     */
-    async createUser(username, password) {
-        try {
-            let currDate = new Date().toISOString().slice(0, 10);
-            let cardListObj = await getStartingCards();
-            let friendListObj = [];
-            let newUser = {
-                userName: username,
-                password: password,
-                dateCreated: currDate,
-                cardList: cardListObj,
-                friendList: friendListObj
-            }
-                    
-            const userAccountsCollection = await userAccounts();
-            const insertUser = await userAccountsCollection.insertOne(newUser)
-            if (!insertUser.acknowledged || !insertUser.insertedId)
-                throw "Could not add user";
+   async createUser(username, password) {
+    try {
+        let currDate = new Date().toISOString().slice(0, 10);
+        let cardListObj = await getStartingCards();
+        let friendListObj = [];
+        let friendRequestsObj = []; // New field to store pending friend requests
+        let newUser = {
+            userName: username,
+            password: password,
+            dateCreated: currDate,
+            cardList: cardListObj,
+            friendList: friendListObj,
+            friendRequests: friendRequestsObj // Add friendRequests field to the user schema
         }
-        catch (e) {
-            console.log(e)
-        }
+
+        const userAccountsCollection = await userAccounts();
+        const insertUser = await userAccountsCollection.insertOne(newUser)
+        if (!insertUser.acknowledged || !insertUser.insertedId)
+            throw "Could not add user";
     }
+    catch (e) {
+        console.log(e)
+    }
+
+    //search user from userAccounts collection
+} async searchUsers(usernameQuery) {
+    try {
+        const userAccountsCollection = await userAccounts();
+        const regex = new RegExp(usernameQuery, 'i'); // Case-insensitive search
+        const users = await userAccountsCollection.find({ userName: regex }).toArray();
+        return users;
+    } catch (e) {
+        console.log(e);
+        return [];
+    }
+}
+
+//send friend request 
+async sendFriendRequest(senderUsername, receiverUsername) {
+    try {
+        const userAccountsCollection = await userAccounts();
+        const senderUser = await userAccountsCollection.findOne({ userName: senderUsername });
+        const receiverUser = await userAccountsCollection.findOne({ userName: receiverUsername });
+
+        if (!senderUser || !receiverUser) {
+            throw "Sender or receiver user not found";
+        }
+
+        // Add the receiver's username to the sender's friendRequests
+        senderUser.friendRequests.push(receiverUsername);
+
+        // Save the updated sender user object
+        await userAccountsCollection.updateOne({ userName: senderUsername }, { $set: { friendRequests: senderUser.friendRequests } });
+
+        console.log(`${senderUsername} sent a friend request to ${receiverUsername}`);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+//Adding accept friend request functionality 
+
+async acceptFriendRequest(receiverUsername, senderUsername) {
+    try {
+        const userAccountsCollection = await userAccounts();
+        const receiverUser = await userAccountsCollection.findOne({ userName: receiverUsername });
+
+        if (!receiverUser) {
+            throw "Receiver user not found";
+        }
+
+        // Remove sender's username from receiver's friendRequests
+        receiverUser.friendRequests = receiverUser.friendRequests.filter(request => request !== senderUsername);
+
+        // Add sender's username to receiver's friendList
+        receiverUser.friendList.push(senderUsername);
+
+        // Save the updated receiver user object
+        await userAccountsCollection.updateOne({ userName: receiverUsername }, { $set: { friendList: receiverUser.friendList, friendRequests: receiverUser.friendRequests } });
+
+        console.log(`${receiverUsername} accepted friend request from ${senderUsername}`);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
 }
 
 
