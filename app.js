ab3705786071e0ae8df566e30f0c3880b16e8f0e
Jay Patel <jaypat0885@gmail.com>
2024-04-23 18:58:23 -0500

    Fixed entire middleware and added more styling

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 import {
   dbConnection,
   closeConnection,
@@ -10,7 +9,7 @@ import session from "express-session";
 import exphbs from "express-handlebars";
 const app = express();
 import configRoutes from "./routes/index.js";
-import { createUserTest } from "./test/createUserTest.js"
+import { createUserTest } from "./test/createUserTest.js";
 
 app.use(
   session({
@@ -22,6 +21,10 @@ app.use(
 );
 
 app.use("/public", express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 try {
   await dbConnection();
@@ -30,52 +33,109 @@ try {
   process.exit(1);
 }
 console.log("Connected to MongoDB");
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
-app.engine("handlebars", exphbs.engine({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
 
 app.use((req, res, next) => {
-  const isAuthenticated = req.session.user
+  const isAuthenticated = req.session.user && req.session.user;
+  const userRole = isAuthenticated
     ? "Authenticated User"
     : "Non-Authenticated User";
   console.log(
     `[${new Date().toUTCString()}]: ${req.method} ${
       req.originalUrl
-    } (${isAuthenticated})`
+    } (${userRole})`
   );
-  next();
-});
 
-configRoutes(app);
-
-app.get("/", (req, res, next) => {
-  if (req.session.user) {
-    if (req.session.user.role === "admin") {
-      res.redirect("/admin");
-    } else if (req.session.user.role === "user") {
-      res.redirect("/protected");
+  if (req.originalUrl === "/") {
+    if (isAuthenticated) {
+      return res.redirect("/protected");
+    } else {
+      console.log("redirecing to login");
+      return res.redirect("/login");
     }
   } else {
-    res.redirect("/login");
+    next();
   }
 });
 
-app.use((req, res, next) => {
-  res.status(404).render("/error", { error: "404: Page Not Found" });
+app.use("/login", (req, res, next) => {
+  if (req.method === "GET") {
+    const isAuthenticated = req.session && req.session.user;
+    if (isAuthenticated) {
+      return res.redirect("/protected");
+    }
+  }
+  next();
+});
+
+app.use("/register", (req, res, next) => {
+  if (req.method === "GET") {
+    const isAuthenticated = req.session && req.session.user;
+    if (isAuthenticated) {
+      console.log("going to redirect to protected");
+      return res.redirect("/protected");
+    }
+  }
+  next();
+});
+
+app.use("/trade", (req, res, next) => {
+  if (req.method === "GET") {
+    const isAuthenticated = req.session && req.session.user;
+    if (!isAuthenticated) {
+      return res.redirect("/login");
+    }
+  }
+  next();
+});
+
+app.use("/trade/:userName", (req, res, next) => {
+  if (req.method === "GET") {
+    const isAuthenticated = req.session && req.session.user;
+    if (!isAuthenticated) {
+      return res.redirect("/login");
+    }
+  }
+  next();
+});
+
+app.use("/viewtrades", (req, res, next) => {
+  if (req.method === "GET") {
+    const isAuthenticated = req.session && req.session.user;
+    if (!isAuthenticated) {
+      return res.redirect("/login");
+    }
+  }
+  next();
+});
+
+app.use("/protected/:id", (req, res, next) => {
+  if (req.method === "GET") {
+    const isAuthenticated = req.session && req.session.user;
+    if (!isAuthenticated) {
+      return res.redirect("/login");
+    }
+  } else {
+    next();
+  }
+  next();
+});
+
+app.use("/logout", (req, res, next) => {
+  if (!req.session || !req.session.user) {
+    return res.redirect("/login");
+  }
+  next();
 });
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
 
+// runs every 5 minutes
+configRoutes(app);
 await pokeMongo.loadAllCards();
 
-// runs every 5 minutes
 setInterval(() => pokeMongo.growCollection(), 300000);
 
 //uncomment this to seed user accounts
 //createUserTest();
-
